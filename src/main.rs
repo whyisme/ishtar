906be9ed57f9c67a61cc78dc49f42efd4518f5ab
ambercane <whyisme@hotmail.com>
2021-04-12 17:08:59 +0800

    daily

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello Ishtar!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
@@ -10,5 +12,14 @@ pub extern "C" fn _start() -> ! {
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    
     loop{}
 }
