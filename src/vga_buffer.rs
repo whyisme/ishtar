83083f16dac29ce1d51920378c01a2f4c34d760c
ambercane <whyisme@hotmail.com>
2021-04-18 19:02:01 +0800

    double fault exceptions

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -141,3 +141,25 @@ pub fn _print(args: fmt::Arguments) {
   use core::fmt::Write;
   WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_simple() {
+  println!("test_println_simple ishtar");
+}
+
+#[test_case]
+fn test_println_many() {
+  for _ in 0..200 {
+    println!("test_println_many ishtar");
+  }
+}
+
+#[test_case]
+fn test_println_output() {
+  let s = "Ishtar comes from the Heaven.";
+  println!("{}", s);
+  for (i, c) in s.chars().enumerate() {
+    let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+    assert_eq!(char::from(screen_char.ascii_character), c);
+  }
+}
