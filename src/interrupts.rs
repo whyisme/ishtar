511914980487073f3a243751a8945f47d99c0bdc
ambercane <whyisme@hotmail.com>
2021-04-25 17:25:39 +0800

    daily

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,6 +3,13 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
 use lazy_static::lazy_static;
 use crate::gdt;
+use pic8259_simple::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 lazy_static!{
   static ref IDT: InterruptDescriptorTable = {
@@ -25,6 +32,22 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
   panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+  Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+  fn as_u8(self) -> u8 {
+    self as u8
+  }
+
+  fn as_usize(self) -> usize {
+    usize::from(self.as_u8())
+  }
 }
 
 #[test_case]
